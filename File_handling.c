1d8ac54e39d8d48ce9b6059ab0e8b6286157ea06
Adroeed100 <muhammedthanseef7@gmail.com>
2025-06-30 15:58:38 +0530

    New Features

--- File_handling.c
+++ File_handling.c
@@ -1,35 +1,52 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
+#include<ctype.h>
 
 int main() {
-    
     while(1) {
-        char c[50];
+        char input[100];
+        char command[20];
+        char filename[80];
+
         printf(">>> ");
-        scanf("%s", &c);
-    
-        if(strcmpi(c,"mkfile")==0)
-        {
-            FILE *f;
+        fgets(input, sizeof(input), stdin);
+        input[strcspn(input, "\n")] = 0;
 
-            f = fopen("welcome.txt", "w");
-            if (f == NULL) {
-                perror("Error");
-                return 1; 
+        int count = sscanf(input, "%s %s", command, filename);
+
+        for(int i = 0; i < command[i] != '\0'; i++) {
+            command[i] = tolower(command[i]);
+        }
+
+        if(count >= 1 && strcmp(command, "mkfile") == 0 ) {
+            if(count<2) {
+                printf("Error: Please provide appropriate filename\n");
+                continue;
             }
-        
-            const char *text = "Hello! nigga Wassup";
-            fwrite(text, sizeof(char), strlen(text), f);
-        
-            fclose(f);
-        
-            printf("File has been created successfully\n");   
-        }
-        else if(strcmpi(c,"exit")==0)
-        {
-            return 1;
+
+        FILE *f  = fopen(filename, "w");
+        if(f == NULL) {
+            perror("Error creating file");
+            continue;
         }
 
-}
+        char text[500];
+        printf("Enter the text \n");
+        fgets(text, sizeof(text), stdin);
+        text[strcspn(text, "\n")] = 0;
+        fwrite(text, sizeof(char), strlen(text), f);
+        fclose(f);
+
+        printf("File '%s' has been created\n", filename);
+
+
+    } else if (strcmp(command, "exit") == 0) {
+        break;
+    } else {
+        printf("Unknown command.\n");
+    }
+
+    }
+    return 0;
 }
