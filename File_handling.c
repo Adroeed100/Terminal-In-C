9c1b15d95cca9d8c3eb064ec46fb854a4d63f6ce
Adroeed100 <muhammedthanseef7@gmail.com>
2025-07-02 10:18:21 +0530

    New Features

--- File_handling.c
+++ File_handling.c
@@ -1,10 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <dirent.h> 
+#include "uses.h"
 
-int main() {
-    while(1) {
+int main()
+{
+    while (1)
+    {
         char input[100];
         char command[20];
         char filename[80];
@@ -15,38 +19,67 @@ int main() {
 
         int count = sscanf(input, "%s %s", command, filename);
 
-        for(int i = 0; i < command[i] != '\0'; i++) {
+        for (int i = 0; i < command[i] != '\0'; i++)
+        {
             command[i] = tolower(command[i]);
         }
 
-        if(count >= 1 && strcmp(command, "mkfile") == 0 ) {
-            if(count<2) {
+        if (count >= 1 && strcmp(command, "mkfile") == 0) {
+        {
+            if (count < 2)
+            {
                 printf("Error: Please provide appropriate filename\n");
                 continue;
             }
-
-        FILE *f  = fopen(filename, "w");
-        if(f == NULL) {
-            perror("Error creating file");
-            continue;
         }
-
         char text[500];
         printf("Enter the text \n");
         fgets(text, sizeof(text), stdin);
         text[strcspn(text, "\n")] = 0;
-        fwrite(text, sizeof(char), strlen(text), f);
-        fclose(f);
+        
+        if (strlen(text) == 0)
+        {
+            printf("File was not created (no text provided) \n");
+            continue;
+        }
+        else
+        {
+            FILE *f = fopen(filename, "w");
+            if (f == NULL)
+            {
+                perror("Error creating file");
+                continue;
+            }
+            fwrite(text, sizeof(char), strlen(text), f);
+            fclose(f);
+            printf("File '%s' has been created\n", filename);
+        }
+    }
+    else if(strcmp(command, "ls") == 0)
+    {
+        struct dirent *de;
+        DIR *dr = opendir(".");
 
-        printf("File '%s' has been created\n", filename);
+        if(dr == NULL) {
+            perror("No file in the directory");
+            continue;
+        }
 
+        while((de = readdir(dr)) != NULL) 
+        {
+            printf("%s\n", de->d_name);
+        }
 
-    } else if (strcmp(command, "exit") == 0) {
+        closedir(dr);
+    }    
+    else if (strcmp(command, "exit") == 0)
+    {
         break;
-    } else {
+    }
+    else
+    {
         printf("Unknown command.\n");
     }
-
-    }
-    return 0;
+}
+return 0;
 }
