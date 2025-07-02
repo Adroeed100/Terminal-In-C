c7c64b212fc3544b892d97602890b3088a681d57
Adroeed100 <muhammedthanseef7@gmail.com>
2025-07-02 15:31:11 +0530

    New Features

--- File_handling.c
+++ File_handling.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <dirent.h> 
-#include "uses.h"
+#include <dirent.h>
 
 int main()
 {
@@ -19,67 +18,112 @@ int main()
 
         int count = sscanf(input, "%s %s", command, filename);
 
-        for (int i = 0; i < command[i] != '\0'; i++)
+        for (int i = 0; i < command[i] != '\0'; i++) //removes case sensitivity
         {
             command[i] = tolower(command[i]);
         }
 
-        if (count >= 1 && strcmp(command, "mkfile") == 0) {
+        if (count >= 1 && strcmp(command, "mkfile") == 0) //creates a file
         {
-            if (count < 2)
             {
-                printf("Error: Please provide appropriate filename\n");
-                continue;
+                if (count < 2)
+                {
+                    printf("Error: Please provide appropriate filename\n");
+                    continue;
+                }
             }
-        }
-        char text[500];
-        printf("Enter the text \n");
-        fgets(text, sizeof(text), stdin);
-        text[strcspn(text, "\n")] = 0;
-        
-        if (strlen(text) == 0)
-        {
-            printf("File was not created (no text provided) \n");
-            continue;
-        }
-        else
-        {
+
             FILE *f = fopen(filename, "w");
             if (f == NULL)
             {
                 perror("Error creating file");
                 continue;
             }
+
+            char text[500];
+            printf("Enter the text \n");
+            fgets(text, sizeof(text), stdin);
+            text[strcspn(text, "\n")] = 0;
+
             fwrite(text, sizeof(char), strlen(text), f);
             fclose(f);
             printf("File '%s' has been created\n", filename);
         }
-    }
-    else if(strcmp(command, "ls") == 0)
-    {
-        struct dirent *de;
-        DIR *dr = opendir(".");
-
-        if(dr == NULL) {
-            perror("No file in the directory");
-            continue;
-        }
-
-        while((de = readdir(dr)) != NULL) 
+        else if (strcmp(command, "ls") == 0) //displays the files inside a directory 
         {
-            printf("%s\n", de->d_name);
-        }
+            struct dirent *de;
+            DIR *dr = opendir(".");
 
-        closedir(dr);
-    }    
-    else if (strcmp(command, "exit") == 0)
-    {
-        break;
+            if (dr == NULL)
+            {
+                perror("no files in drirectory");
+                continue;
+            }
+
+            while ((de = readdir(dr)) != NULL)
+            {
+                printf("%s\t", de->d_name);
+            }
+            printf("\n");
+
+            closedir(dr);
+        }
+        else if (count >= 1 && strcmp(command, "mkdir") == 0) //creates a directory 
+        {
+            if (count < 2)
+            {
+                printf("Please provide appropriate folder name\n");
+                continue;
+            }
+            else
+            {
+                if (mkdir(filename) == 0)
+                {
+                    printf("Foler '%s' created successfully\n", filename);
+                }
+                else
+                {
+                    printf("Folder not created not created\n");
+                }
+            }
+        } 
+        else if(count>=1 && strcmp(command, "rmdir")==0) //removes directory
+        {
+            if(count<2) {
+                printf("Error: Enter appropriate directory name\n");
+                continue;
+            }
+
+            if(rmdir(filename) == 0) {
+                printf("folder '%s' removed\n", filename);
+            } else {
+                printf("Error: Cannot remove folder\n");
+                continue;
+            }
+
+        }
+        else if(count>=1 && strcmp(command, "rmfile")==0) //removes a file
+        {
+            if(count<2) {
+                printf("Error: Enter appropriate filename\n");
+                continue;
+            }
+
+            if(remove(filename)==0) {
+                printf("File '%s' removed\n", filename);
+            } else {
+                printf("Error: Cannot remove file\n");
+            }
+
+        }
+        else if (strcmp(command, "exit") == 0) //exits the terminal 
+        {
+            break;
+        }
+        else //if the command is invalid
+        {
+            printf("Unknown command.\n");
+        }
     }
-    else
-    {
-        printf("Unknown command.\n");
-    }
-}
-return 0;
+    return 0;
 }
---
+++ uses.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// shit used for listing files lmao
+void list()
+{
+    
+}
+
+// #include <stdio.h>
+// #include <direct.h>
+
+// int main()
+// {
+//     const char *dirname = "my_directory";
+
+//     if (_mkdir(dirname) == 0)
+//     {
+//         printf("Directory created successfully.\n");
+//     }
+//     else
+//     {
+//         perror("mkdir failed");
+//     }
+
+//     return 0;
+// }
