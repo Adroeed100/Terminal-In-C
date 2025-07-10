7ba76015e3ad15f6b4db5f6903af9059472bde3c
Adroeed100 <muhammedthanseef7@gmail.com>
2025-07-10 21:06:58 +0530

    modularization

--- File_handling.c
+++ File_handling.c
@@ -2,178 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <dirent.h>
-#include <unistd.h>
+#include "uses.h"
 
 int main()
 {
     while (1)
     {
-        char input[100];
-        char command[20];
-        char filename[80];
-
-        printf(">>> ");
+        char input[100], command[10], arg[100];
+        
+        char cwd[1024];
+        printf(getcwd(cwd, sizeof(cwd)));
+        printf(">>>");
         fgets(input, sizeof(input), stdin);
         input[strcspn(input, "\n")] = 0;
 
-        int count = sscanf(input, "%s %s", command, filename);
+        int count = sscanf(input, "%s %s", command, arg);
 
-        for (int i = 0; i < command[i] != '\0'; i++) // removes case sensitivity
+        int flag = 0;
+        for (int i = 0; captian[i].command != NULL; i++)
         {
-            command[i] = tolower(command[i]);
-        }
-
-        if (count >= 1 && strcmp(command, "mkfile") == 0) // creates a file
-        {
+            if (strcmp(command, captian[i].command) == 0)
             {
-                if (count < 2)
-                {
-                    printf("Error: Please provide appropriate filename\n");
-                    continue;
-                }
-            }
-
-            FILE *f = fopen(filename, "w");
-            if (f == NULL)
-            {
-                perror("Error creating file");
-                continue;
-            }
-
-            char text[500];
-            printf("Enter the text \n");
-            fgets(text, sizeof(text), stdin);
-            text[strcspn(text, "\n")] = 0;
-
-            fwrite(text, sizeof(char), strlen(text), f);
-            fclose(f);
-            printf("File '%s' has been created\n", filename);
-        }
-        else if (strcmp(command, "ls") == 0) // displays the files inside a directory
-        {
-            struct dirent *de;
-            DIR *dr = opendir(".");
-
-            if (dr == NULL)
-            {
-                perror("no files in drirectory");
-                continue;
-            }
-
-            while ((de = readdir(dr)) != NULL)
-            {
-                printf("%s\t", de->d_name);
-            }
-            printf("\n");
-
-            closedir(dr);
-        }
-        else if (count >= 1 && strcmp(command, "mkdir") == 0) // creates a directory
-        {
-            if (count < 2)
-            {
-                printf("Please provide appropriate folder name\n");
-                continue;
-            }
-            else
-            {
-                if (mkdir(filename) == 0)
-                {
-                    printf("Foler '%s' created successfully\n", filename);
-                }
-                else
-                {
-                    printf("Folder not created not created\n");
-                }
+                captian[i].sfunc(count >= 2 ? arg : NULL);
+                flag = 1;
+                break;
             }
         }
-        else if (count >= 1 && strcmp(command, "rmdir") == 0) // removes directory
-        {
-            if (count < 2)
+          if (!flag)
             {
-                printf("Error: Enter appropriate directory name\n");
-                continue;
+                printf("Unknown command\n");
             }
-
-            if (rmdir(filename) == 0)
-            {
-                printf("folder '%s' removed\n", filename);
-            }
-            else
-            {
-                printf("Error: Cannot remove folder\n");
-                continue;
-            }
-        }
-        else if (count >= 1 && strcmp(command, "rmfile") == 0) // removes a file
-        {
-            if (count < 2)
-            {
-                printf("Error: Enter appropriate filename\n");
-                continue;
-            }
-
-            if (remove(filename) == 0)
-            {
-                printf("File '%s' removed\n", filename);
-            }
-            else
-            {
-                printf("Error: Cannot remove file\n");
-            }
-        }
-        else if (count >= 1 && strcmp(command, "show") == 0) // display the contents of file
-        {
-            if (count < 2)
-            {
-                printf("Please provide a file name to shou\n");
-                continue;
-            }
-            FILE *f = fopen(filename, "r");
-            if (f == NULL)
-            {
-                perror("error opening the file\n");
-                continue;
-            }
-            char ch;
-            while ((ch = fgetc(f)) != EOF)
-            {
-                putchar(ch);
-            }
-            printf("\n\n");
-            fclose(f);
-        }
-        else if(count >= 1 && strcmp(command, "cd") == 0) // changing the directory
-        {
-            if(count<2) {
-                fprintf(stderr, "please select a valid directory\n");
-                continue;
-            }
-            if(chdir(filename) != 0) {
-                perror("cd error\n");
-            }
-        }
-        else if(strcmp(command, "whereami") == 0) //to display the current directory
-        {
-            char cwd[1024];
-            if(getcwd(cwd, sizeof(cwd)) != NULL)
-            {
-                printf("%s\n",cwd);
-            }
-            else
-            {
-                perror("getcwd() error");
-            }
-        }
-        else if (strcmp(command, "exit") == 0) // exits the terminal
-        {
-            break;
-        }
-        else // if the command is invalid
-        {
-            printf("Unknown command.\n");
-        }
     }
-    return 0;
 }
